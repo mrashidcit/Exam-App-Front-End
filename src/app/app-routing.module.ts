91623cb79d62ca8cdd2dca5f19bdedec421bc3e7
mrashidcit <mrashidcit@gmail.com>
2017-07-11 07:08:04 +0500

    Home is working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,17 @@ import {NewQuestionComponent} from "./admin/new-question/new-question.component"
 //import {QuestionListComponent} from "./questions/question-list/question-list.component";
 import {QuizParentComponent} from "./quiz/quiz-parent/quiz-parent.component";
 import {CanDeactivateGuard} from "./services/can-deactivate-guard.service";
+import { HomeComponent } from "./home/home.component";
+
+
 
 
 const appRoutes: Routes = [
-    {path: '', redirectTo:'/quiz', pathMatch:"full" },
+    //{path: '', redirectTo:'/quiz', pathMatch:"full" },
+    {
+        path: '',
+        component: HomeComponent
+    }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,28 @@ import { MaterialModule, MdNativeDateModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { GradeListComponent } from './grades/grade-list/grade-list.component';
 import {AppRoutingModule} from "./app-routing.module";
-import {GradeService} from "./services/grade.service";
-import {SubjectService} from "./services/subject.service";
+
+
 import { GradeComponent } from './grades/grade/grade.component';
-import {QuestionService} from "./question.service";
+
+import { AdminModule } from './admin/admin.module';
+
+import { LoginRoutingModule } from './login/login-routing.module';
+import { LoginComponent } from './login/login.component';
+import { QuestionService } from './question.service';
+import { QuestionsModule } from './questions/questions.module';
 import { QuizParentComponent } from './quiz/quiz-parent/quiz-parent.component';
+import { QuizRoutingModule } from './quiz/quiz-routing.module';
+import { QuizModule } from './quiz/quiz.module';
 import { QuizComponent } from './quiz/quiz/quiz.component';
 import { ShowResultComponent } from './quiz/show-result/show-result.component';
-import {AdminModule} from "./admin/admin.module";
-import {QuestionsModule} from "./questions/questions.module";
-import {QuizModule} from "./quiz/quiz.module";
-import { LoginComponent } from './login/login.component';
-import {LoginRoutingModule} from "./login/login-routing.module";
-import {DialogService} from "./services/dialog.service";
-import {QuizRoutingModule} from "./quiz/quiz-routing.module";
-import { MaterialComponentModule } from "app/shared/material-component/material-component.module";
+import { DialogService } from './services/dialog.service';
+import { GradeService } from './services/grade.service';
+import { SubjectService } from './services/subject.service';
+import { MaterialComponentModule } from './shared/material-component/material-component.module';
+import { SharedModule } from './shared/shared.module';
 import 'HammerJS';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
@@ -31,6 +37,7 @@ import 'HammerJS';
     GradeListComponent,
     GradeComponent,
     LoginComponent,
+    HomeComponent,
 
   ],
   imports: [
@@ -38,6 +45,7 @@ import 'HammerJS';
     FormsModule,
     HttpModule,
     MaterialComponentModule,
+    SharedModule,
     LoginRoutingModule,
     QuizModule,
     QuestionsModule,
